5c2f861ff97ae1fcbc07fb8ccf1bee876f2b41cd
FaridaAdamuMagaji <faridaadamumagaji@gmail.com>
2025-04-10 04:50:50 -0700

    added multer

--- Backend/server.js
+++ Backend/server.js
@@ -29,7 +29,6 @@ mongoose.connect(mongo)
     .then(() => console.log('MongoDB connected successfully'))
     .catch(err => console.error('MongoDB connection error:', err));
 
-module.exports = mongoose;
 
 
 // Start the server
