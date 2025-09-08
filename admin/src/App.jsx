c06fcf1dc5fbe531031c6ec7ad96e9d16c6c1b95
FaridaAdamuMagaji <faridaadamumagaji@gmail.com>
2025-09-08 09:05:27 -0700

    added render link

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,23 +1,3 @@
-/*import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Flowers from "./pages/flowers";
-import AddFlower from "./pages/addFlower";
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/flowers" element={<Flowers />} />
-          <Route path="/add-flower" element={<AddFlower />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
-
-export default App;*/
-
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Flowers from "./pages/flowers";
