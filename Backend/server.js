63b3aad2df072d6eb871a275d69d3727c8149042
FaridaAdamuMagaji <faridaadamumagaji@gmail.com>
2025-09-11 03:51:48 -0700

    edited routes

--- Backend/server.js
+++ Backend/server.js
@@ -8,23 +8,11 @@ const flowerRoutes = require('./routes/flowerRoutes');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 
 // Middleware
-/*app.use(cors());*/
-
-// ✅ This is the only CORS config you need
-app.use(cors({
-  origin: [
-    "http://localhost:5173",          // for local dev
-    "https://flower-admin-4l4p.onrender.com"  // your deployed admin
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
-
 app.use(express.json());
-/*app.use("/assets", express.static(path.join(process.cwd(), "assets")));// Serve uploaded images*/
+app.use(cors());
 
 app.use("/assets", express.static(path.join(__dirname, "assets")));
 
@@ -58,6 +46,7 @@ app.get('/users', async (req, res) => {
 
 
 // Start the server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
